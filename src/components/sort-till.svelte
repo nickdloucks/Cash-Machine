b7848fddee20ebbe6b6a9f72a99b5f2d460a10cf
Nick <nickdloucks@outlook.com>
2022-05-07 15:02:33 -0700

    add grid styling to stylesheet

--- src/components/sort-till.svelte
+++ src/components/sort-till.svelte
@@ -6,11 +6,11 @@
     //     // search cashInTill global var for the slot name
     //     // when found, update the value of the slot.
     // }
-    
+
     // function makeChange(): void{ //use as event listener for a button
     //     // useTill(,, cashInTill);
     // }
-    
+
     // document.getElementById('#calc-change')?.addEventListener('click', makeChange);
     // // ^ calculate change given when user clicks the "Make Change" button
 </script>
@@ -23,17 +23,17 @@
     
     <h4>Cash in drawer:</h4>
     // DYNAMICALLY RENDER THIS LIST FROM THE LIST OF COIN/BILL TYPES WHICH WILL BE IN THE STORE
-    <ol> 
-        <li><label for="pennies">pennies</label><input type="number" name="pennies"></li>
-        <li><label for="nickles">nickles</label><input type="number" name="nickles"></li>
-        <li><label for="dimes">dimes</label><input type="number" name="dimes"></li>
-        <li><label for="quarters">quarters</label><input type="number" name="quarters"></li>
-        <li><label for="1-dollar-bills">$1 bills</label><input type="number" name="1-dollar-bills"></li>
-        <li><label for="5-dollar-bills">$5 bills</label><input type="number" name="5-dollar-bills"></li>
-        <li><label for="10-dollar-bills">$10 bills</label><input type="number" name="10-dollar-bills"></li>
-        <li><label for="20-dollar-bills">$20 bills</label><input type="number" name="20-dollar-bills"></li>
-        <li><label for="50-dollar-bills">$50 bills</label><input type="number" name="50-dollar-bills"></li>
-        <li><label for="100-dollar-bills">$100 bills</label><input type="number" name="100-dollar-bills"></li>
+    <ol class="grid-container"> 
+        <li class="grid-item"><label for="pennies">pennies</label><input type="number" name="pennies"></li>
+        <li class="grid-item"><label for="nickles">nickles</label><input type="number" name="nickles"></li>
+        <li class="grid-item"><label for="dimes">dimes</label><input type="number" name="dimes"></li>
+        <li class="grid-item"><label for="quarters">quarters</label><input type="number" name="quarters"></li>
+        <li class="grid-item"><label for="1-dollar-bills">$1 bills</label><input type="number" name="1-dollar-bills"></li>
+        <li class="grid-item"><label for="5-dollar-bills">$5 bills</label><input type="number" name="5-dollar-bills"></li>
+        <li class="grid-item"><label for="10-dollar-bills">$10 bills</label><input type="number" name="10-dollar-bills"></li>
+        <li class="grid-item"><label for="20-dollar-bills">$20 bills</label><input type="number" name="20-dollar-bills"></li>
+        <li class="grid-item"><label for="50-dollar-bills">$50 bills</label><input type="number" name="50-dollar-bills"></li>
+        <li class="grid-item"><label for="100-dollar-bills">$100 bills</label><input type="number" name="100-dollar-bills"></li>
     </ol>
     <button id="calc-change" class="good-button">Make Change</button>
 
