bdd8f8fe1c16959dac85190ad73cbc4ba03827e0
Nick <nickdloucks@outlook.com>
2022-05-07 12:02:09 -0700

    number input box component

---
+++ src/components/number-input-box.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let value: number = 0.1;
+    export let name: string;
+    export const minimum: number = 0;
+</script>
+
+<input type="number" name={name} placeholder={String(value)} value={value}>
--- src/components/sort-till.svelte
+++ src/components/sort-till.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-
+    import NumberBox from './number-input-box.svelte';
 </script>
 
 <form>
     
-    <label for="price">Price:</label><input name="price">
+    <label for="price">Price:</label><svelte:component this={NumberBox} value={0} name={'price'}></svelte:component>
     
-    <label for="paid">Cash given:</label><input name="paid">
+    <label for="paid">Cash given:</label><svelte:component this={NumberBox} value={0} name={'paid'}></svelte:component>
     
     <h4>Cash in drawer:</h4>
-
-    <label for="pennies">pennies</label><input type="number" name="pennies">
-    <label for="nickles">nickles</label><input type="number" name="nickles">
-    <label for="dimes">dimes</label><input type="number" name="dimes">
-    <label for="quarters">quarters</label><input type="number" name="quarters">
-    <label for="1-dollar-bills">$1 bills</label><input type="number" name="1-dollar-bills">
-    <label for="5-dollar-bills">$5 bills</label><input type="number" name="5-dollar-bills">
-    <label for="10-dollar-bills">$10 bills</label><input type="number" name="10-dollar-bills">
-    <label for="20-dollar-bills">$20 bills</label><input type="number" name="20-dollar-bills">
-    <label for="50-dollar-bills">$50 bills</label><input type="number" name="50-dollar-bills">
-    <label for="100-dollar-bills">$100 bills</label><input type="number" name="100-dollar-bills">
-
+    // DYNAMICALLY RENDER THIS LIST FROM THE LIST OF COIN/BILL TYPES WHICH WILL BE IN THE STORE
+    <ol> 
+        <li><label for="pennies">pennies</label><input type="number" name="pennies"></li>
+        <li><label for="nickles">nickles</label><input type="number" name="nickles"></li>
+        <li><label for="dimes">dimes</label><input type="number" name="dimes"></li>
+        <li><label for="quarters">quarters</label><input type="number" name="quarters"></li>
+        <li><label for="1-dollar-bills">$1 bills</label><input type="number" name="1-dollar-bills"></li>
+        <li><label for="5-dollar-bills">$5 bills</label><input type="number" name="5-dollar-bills"></li>
+        <li><label for="10-dollar-bills">$10 bills</label><input type="number" name="10-dollar-bills"></li>
+        <li><label for="20-dollar-bills">$20 bills</label><input type="number" name="20-dollar-bills"></li>
+        <li><label for="50-dollar-bills">$50 bills</label><input type="number" name="50-dollar-bills"></li>
+        <li><label for="100-dollar-bills">$100 bills</label><input type="number" name="100-dollar-bills"></li>
+    </ol>
     <button id="calc-change" class="good-button">Make Change</button>
 
 </form>
