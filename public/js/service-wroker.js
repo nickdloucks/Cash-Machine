eb3c7ad3fe87ef16e0928df7689e60f79f29cd24
Nick <nickdloucks@outlook.com>
2022-05-07 10:27:02 -0700

    learning about service workers, using MDN tutorial

---
+++ public/js/service-wroker.js
@@ -0,0 +1,27 @@
+// code from MDN article on service workers: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+
+const registerServiceWorker = async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        '/sw-test/sw.js',
+        {
+          scope: '/sw-test/',
+        }
+      );
+      if (registration.installing) {
+        console.log('Service worker installing');
+      } else if (registration.waiting) {
+        console.log('Service worker installed');
+      } else if (registration.active) {
+        console.log('Service worker active');
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
+// ...
+
+registerServiceWorker();
