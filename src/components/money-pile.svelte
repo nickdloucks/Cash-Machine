2f3ed39f2f96b3620f79218fe3f84af1c3d0b71f
Nick <nickdloucks@outlook.com>
2022-05-08 16:44:11 -0700

    styling money pile items

--- src/components/money-pile.svelte
+++ src/components/money-pile.svelte
@@ -1,15 +1,21 @@
-<script>
+<script lang="ts">
     import { cashInTill, MONEY } from '../stores/cash-drawer';
 
     // export let pileType; // the type of money "pile" can be either cashInTill or changePile
     // use destructuring to access bill names/values
     let dollar_value = MONEY[0][1];
+
+    function moneyCharacter(value: number): string{
+        return (value >= 1) ? String.fromCharCode(0x24) : String.fromCharCode(0xA2); // if input value is greater than or equal to 1, 
+        // return the dollar sign character, 
+        // otherwise return the cent character
+    }
 </script>
 
 <h2>MONEY PILE COMPONENT:</h2>
 <ol class="grid-container">
     {#each cashInTill as bill_coin, index} <!-- this should map to the pile type-->
-        <li class="grid-item money-picture">{bill_coin[0].concat(` - $${MONEY[index][1]} X <quantity>`)}</li>
+        <li class="grid-item money-picture">{bill_coin[0].concat(` ${moneyCharacter(MONEY[index][1])} - $${MONEY[index][1]} X <quantity>`)}</li>
     {/each}
 </ol>
 
@@ -25,9 +31,24 @@
         <li class="grid-item"><label class="money-in-label" for="50-dollar-bills">$50 bills</label><input class="money-in" type="number" name="50-dollar-bills"></li>
         <li class="grid-item"><label class="money-in-label" for="100-dollar-bills">$100 bills</label><input class="money-in" type="number" name="100-dollar-bills"></li>
     </ol> -->
+<ol class="grid-container">
+    <li class="grid-item money-picture cent-picture">1&cent;</li>
+    <li class="grid-item money-picture dollar-picture">1&#36;</li>
+</ol>
 
 <style>
     .money-picture{
+        background-color: #cccccc;
+        font-weight: bold;
         width: fit-content;
+        color: #000000;
+    }
+    .cent-picture{
+        background-color: #cccccc;
+        border-radius: 100%;
+    }
+    .dollar-picture{
+        background-color: #008800;
+        border-radius: 2px;
     }
 </style>
