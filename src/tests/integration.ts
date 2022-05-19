9fc8c963a37a496fa2e5cf964494bc422c4f6c7b
Nick <nickdloucks@outlook.com>
2022-05-18 18:49:36 -0700

    start setting up UI integration testing

---
+++ src/tests/integration.ts
@@ -0,0 +1,7 @@
+import {MONEY, cashInTill} from '../stores/cash-drawer';
+
+describe("Integration testing for the cash till program into it's UI.", ()=>{
+    it('Gets and sets store values properly:', ()=>{
+
+    });
+})
