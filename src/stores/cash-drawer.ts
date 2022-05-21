3f3a4c6702737b30c4721dbbd296056bda0b2ddc
Nick <nickdloucks@outlook.com>
2022-05-21 11:49:09 -0700

    doc/comments for drawer store

--- src/stores/cash-drawer.ts
+++ src/stores/cash-drawer.ts
@@ -2,7 +2,9 @@ import { Writable, writable } from 'svelte/store'; // import both the type and t
 import type { writableSlots } from '../global';
 
 export const MONEY: ReadonlyArray<[string, number]> = [
-    // money value data stored in array so the recursive function can process it in order of value
+    // Money value data stored in array so the recursive function can process it in order of value.
+    // The MONEY array represents the value of one instance of a given bill or coin. 
+    // The values are readonly and immutable.
     ['PENNY', 0.01],
     ['NICKEL', 0.05],
     ['DIME', 0.1],
@@ -12,7 +14,7 @@ export const MONEY: ReadonlyArray<[string, number]> = [
     ['TEN', 10.0],
     ['TWENTY', 20.0], // ADD $50 BILL SPOT
     ['ONE HUNDRED', 100.0],
-]; // The MONEY array represents the value of one instance of a given bill or coin
+];
 
 // export let drawerSlots: writableSlots = {
 //     tillPennies: writable(0.01),
@@ -25,6 +27,8 @@ export const MONEY: ReadonlyArray<[string, number]> = [
 //     tillTwenties: writable(20.0),
 //     tillHundreds: writable(100.0)
 // }
+
+// These Svelte store writables represent stateful slots in the cash drawer. They are readable and writable.
 export const tillPennies = writable(0.01);
 export const tillNickles = writable(0.5);
 export const tillDimes = writable(0.1);
@@ -37,7 +41,8 @@ export const tillHundreds = writable(100.0);
 
 export const drawerSlots = [tillPennies, tillNickles, tillDimes, tillQuarters, tillOnes, tillFives, tillTens, tillTwenties, tillHundreds];
 
-export const cashInTill: Array<[string, (Writable<number> | number)]> = [ // Array for keeping track of the till state. // : Array<[string,(number | Writable<number>)]> | []
+export const cashInTill: Array<[string, (Writable<number> | number)]> = [ 
+    // Array for keeping track of the till state. // : Array<[string,(number | Writable<number>)]> | []
     ['PENNY', tillPennies],
     ['NICKEL', tillNickles],
     ['DIME', tillDimes],
@@ -49,7 +54,8 @@ export const cashInTill: Array<[string, (Writable<number> | number)]> = [ // Arr
     ['ONE HUNDRED', tillHundreds],
 ];
 
-export let changePile: Array<[string, (Writable<number> | number)]> | []; // Array representing change given to customer
+// Array representing change given to customer after a sale:
+export let changePile: Array<[string, (Writable<number> | number)]> | []; 
 
 //  to-do: factory function that generates a random till state and writes to localStorage with values:
 // localStorage.setItem('cashInTill', JSON.stringify(cashInTill));
