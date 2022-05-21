2f7f7c72c49e2ceb7e4a1f0e0decc51a6897895e
Nick <nickdloucks@outlook.com>
2022-05-21 11:48:28 -0700

    add step, minimum input validations to component

--- src/components/number-input-box.svelte
+++ src/components/number-input-box.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
-    export let value: number = 0.1;
+    import type {Writable} from 'svelte/store';
+
+    export let value: number | Writable<number> = 0.1;
     export let name: string; // IS THIS THE NAME OF THE MONEY VALUE FOUND IN THE TILL STORE?
+    export let step: number;
     export const minimum: number = 0;
     // handler function for updating till slot value connected to this input,
     // or figure out how to link it to a devrived store variable
 </script>
 
-<input class="money-in" type="number" name={name} placeholder={String(value)} value={value}>
+<input 
+    class="money-in" 
+    type="number" 
+    name={name} 
+    value={value} 
+    placeholder={String(value)}
+    min={minimum}
+    step={step}>
 
 <style>
     .money-in{
