ca5e0850abd5e8e7a42a1aba5e435c3053144be3
Nick <nickdloucks@outlook.com>
2022-05-23 21:38:11 -0700

    add input validation function, set default val @ 0

--- src/components/number-input-box.svelte
+++ src/components/number-input-box.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import type {Writable} from 'svelte/store';
 
-    let inputBox;
+    let inputBox: HTMLInputElement;
 
     export let drawerSlot: Writable<number>; // configure to be usable by main transaction: use ternary operator
-    export let value: number = 0.1;
+    export let val: number = 0; // Dollar value for this slot; default to an empty slot
     export let name: string; // IS THIS THE NAME OF THE MONEY VALUE FOUND IN THE TILL STORE?
     export let step: number;
     export const minimum: number = 0;
@@ -14,6 +14,11 @@
     // export let editable: boolean = false;
     // handler function for updating till slot value connected to this input,
     // or figure out how to link it to a devrived store variable
+    function validateSlotIn(inputVal: number = val, interval: number = step): void{ // validate user input for the drawer slot
+        if ((inputVal % interval) > 0){ // if user input is not evenly divisible by the money value for the current slot, round down to nearest interval
+            inputBox.value = String(inputVal - (inputVal % interval));
+        }
+    }
 </script>
 
 <input 
@@ -22,10 +27,10 @@
     type="number" 
     name={name}
     bind:value={$drawerSlot} 
-    placeholder={"$ ".concat(String(value))}
+    placeholder={"$ ".concat(String(val))}
     min={minimum}
     step={step}
-    >
+    on:change={validateSlotIn}>
 
 <style>
     .money-in{
