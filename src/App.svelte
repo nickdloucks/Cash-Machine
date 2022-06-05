a00772c2d162705a9306e099307d0ddf9d74b8a2
Nick <nickdloucks@outlook.com>
2022-06-05 15:29:20 -0700

    <changePile>from store as prop ==> MoneyPile comp

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,14 @@
 	import CashPanel from './components/sort-till.svelte';
 	import MoneyPile from './components/money-pile.svelte';
 	import Tracker from './components/drawer-tracker.svelte';
+	import { changePile } from './stores/cash-drawer'; // this is a store value that will be changed based on the result of running the "make-change" program
 	
 </script>
 
 <main>
 	<svelte:component this={TransactionControls}></svelte:component>
 	<svelte:component this={CashPanel}></svelte:component>
-	<svelte:component this={MoneyPile}></svelte:component>
+	<svelte:component this={MoneyPile} pile={changePile}></svelte:component>
 	<svelte:component this={Tracker}></svelte:component>
 </main>
 
