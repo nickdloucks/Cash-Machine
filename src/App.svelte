b874c68d56caf54c3368e9ea43198ab51e511d13
Nick <nickdloucks@outlook.com>
2022-06-08 16:27:20 -0700

    del tracker component: slot-interface tracks $ now

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
 	import TransactionControls from './components/transaction-controls.svelte';
 	import CashPanel from './components/sort-till.svelte';
 	import MoneyPile from './components/money-pile.svelte';
-	import Tracker from './components/drawer-tracker.svelte';
 	import { changePile } from './stores/cash-drawer'; // this is a store value that will be changed based on the result of running the "make-change" program
 	
 </script>
@@ -11,7 +10,6 @@
 	<svelte:component this={TransactionControls}></svelte:component>
 	<svelte:component this={CashPanel}></svelte:component>
 	<svelte:component this={MoneyPile} pile={changePile}></svelte:component>
-	<svelte:component this={Tracker}></svelte:component>
 </main>
 
 <style>
