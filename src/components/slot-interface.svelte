e6951759383a7ea1faa8f339dc147e28d6ca9a69
Nick <nickdloucks@outlook.com>
2022-06-10 16:17:16 -0700

    cleanup, comments, default for editable

--- src/components/slot-interface.svelte
+++ src/components/slot-interface.svelte
@@ -3,20 +3,15 @@
     // import validate function for event handler, test separately
 
     export let drawerSlot: Writable<number>; // configure to be usable by main transaction: use ternary operator  
-
-    export let name: string;
+    export let name: string; // name of the slot, money type
     export let slotUnitVal: number; // the dollar value of ONE bill/coin in this particular drawer slot. EX: a penny slot's "slotUnitVal" would be ($) 0.01
-    export let editable: boolean; // WHAT IF EDITABLE PROP IS NOT PROVIDED??
+    export let editable: boolean = true; // user can make the input readonly; default is editable
     
-
-
     const minimum: number = 0;
     // slot total will initially be 0, while store value for the slot is displayed to side
 
     $: roundedSlot = Math.round(100 * Number($drawerSlot)) / 100 || 0; // Dollar value Total for this slot; default to an empty slot
-    $: slotDisplay = "$ ".concat(String(roundedSlot.toFixed(2)));
-
-
+    $: slotDisplay = "$ ".concat(String(roundedSlot.toFixed(2))); // The slot's dollar value formatted as money. ex: $0.00
     $: validatedTotal = Math.round(roundedSlot / slotUnitVal);
 
     const validate = function(event: any, userInput: number = validatedTotal, unitVal: number = slotUnitVal):void{ // validate user input and alter the value in the drawer store
@@ -27,6 +22,7 @@
     }
 
     const directlyEditable = function(){
+        // handler func for if user sets the editable prop
         return !editable;
     }
 </script>
@@ -47,30 +43,31 @@
             on:input={validate}
             on:change={validate}
             >
-
         <p class="slot-value">{slotDisplay}</p>
     </div>
 </li>
-
-        <!-- <button on:click={validate}>set</button> -->
 
 <style>
     .slot-label{
         width: max-content;
         display: inline-block;
         height: max-content;
+        /* font-size: 1em; */
     }
     .slot-card{
+        display: grid;
         width: min-content;
         background-image:radial-gradient(#556b2f, #008800);
         border-radius: 0.5em;
+        width: 5rem;
+        justify-content: center;
     }
     .money-slot-input{
-        width: 3rem;
+        width: 2.5rem;
         height: min-content;
-        
     }
     .slot-value{
+        /* font-size: 1em; */
         width: max-content;
     }
 </style>
