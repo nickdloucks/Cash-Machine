b7f673f9feade3d57eef0fcccfae81a8e47b0659
Nick <nickdloucks@outlook.com>
2022-06-10 14:23:36 -0700

    remove moneyPile from app while redesigning

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	import TransactionControls from './components/transaction-controls.svelte';
 	import CashDrawer from './components/drawer-interface.svelte';
-	import MoneyPile from './components/money-pile.svelte';
-	import { changePile } from './stores/cash-drawer'; // this is a store value that will be changed based on the result of running the "make-change" program
+	// import MoneyPile from './components/money-pile.svelte';
+	// import { changePile } from './stores/cash-drawer'; // this is a store value that will be changed based on the result of running the "make-change" program
 	
 </script>
 
 <main>
 	<svelte:component this={TransactionControls}></svelte:component>
 	<svelte:component this={CashDrawer}></svelte:component>
-	<svelte:component this={MoneyPile} pile={changePile}></svelte:component>
+	<!-- <svelte:component this={MoneyPile} pile={changePile}></svelte:component> -->
 </main>
 
 <style>
