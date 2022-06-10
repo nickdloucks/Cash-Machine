4883fbe52912628cc2e80054e04a2f307d1bec65
Nick <nickdloucks@outlook.com>
2022-06-10 11:33:37 -0700

    handle underpayment, remove old comments/logs

--- src/make-change.ts
+++ src/make-change.ts
@@ -15,29 +15,26 @@
 import { MONEY } from './stores/cash-drawer';
 import type { MoneyInstance, TillStatus } from './global';
 
-// TODO: change the places where cid is mutated: it needs to be more deliberate with types (like Writable<number>) so it's auto-updating the store
-// maybe pass a copy of the CID array, mutate the copy, then update state in store using the copy
-//HANDLE EDGE CASE IN WHICH NOT ENOUGH MONEY IS GIVENT BY THE CUSTOMER??
- // ADD $50 BILL SPOT
-
 export default function checkCashRegister(price: number, cash: number, cid: Array<MoneyInstance>): TillStatus {
   price = precise(price); // Ensure money params are w/ in desired precision
   cash = precise(cash); // See hoisted function <precise()> below the <MONEY> constant
 
   let stillDue_$ = precise(cash - price); // Init. variable: amount of money the customer is still owed
-  let changePile: Array<MoneyInstance> = []; // itemized breakdown of change to be given to the customer
-  // ========= STANDARD DATA NEEDED ====
 
-  // console.log('hello from main program...')
-  // changes to drawerBuffer work
-  // console.log(...cid)
+  if(stillDue_$ < 0){ // do not process transactions where insufficient payment has been given
+    return  { status: 'INSUFFICIENT_PAYMENT', message: 'Transaction not processed.', change: [] };
+  }
+
+  let changePile: Array<MoneyInstance> = []; // itemized breakdown of change to be given to the customer
+  // ========= ^ STANDARD DATA NEEDED ====
+
   function precise(decimal: number): number {
     return Math.floor(100 * decimal) / 100; // All money values should be given to the nearest hundredth
-    // This function helps since Javascript is not as prcise with decimal numbers as other lanaguages like Python.
+    // This function helps since Javascript is not as prcise with floating point numbers as other lanaguages like Python.
   }
 
   //// Till COUNTING SUBROUTINE:
-  function tillCount(arr2D: Array<MoneyInstance>) {
+  function tillCount(arr2D: Array<MoneyInstance>) { // count up all the cash in the drawer
     let counter: number = 0;
     for (let i = 0; i < arr2D.length; i++) {
       counter += arr2D[i][1] as number;
@@ -52,13 +49,12 @@ export default function checkCashRegister(price: number, cash: number, cid: Arra
   if (totalTill < stillDue_$) { // Not enough money left in the till to make change for this cash amount.
     return { status: 'INSUFFICIENT_FUNDS', change: [] };
   } else if (totalTill === stillDue_$) {
-    // customer is owed the exact ammount of change in the till
+    // When customer is owed the exact ammount of change in the till:
     // give customer all the change and close out the till
-    // let drawerTotal = cid.slice();
     cid.forEach((slot: MoneyInstance)=>{
       // add a copy of entire slot to ChangePile; then set original cid[slot value] to zero
       // BE CAREFUL NOT TO SET BOTH TO ZERO
-      let slotCopy: MoneyInstance = [...slot]; // copy slot, passing items * by value * NOT by reference
+      let slotCopy: MoneyInstance = [...slot]; // copy slot, passing items by *value* NOT by reference
       if(slotCopy[1] > 0){ // only add the copy to the change pile if the slot was not already empty
           changePile.push(slotCopy);
       }
@@ -66,7 +62,7 @@ export default function checkCashRegister(price: number, cash: number, cid: Arra
     });
     console.log('All slots should be empty', 'drawerTotal:', ...cid);
     return { status: 'CLOSED', change: changePile };
-  } else { // when totalTill > stillDue_$
+  } else { // when the amount of money owed to the customer is less than the contents of the drawer:
     ////////////////////////////////////////////////////////////////////////////////////
     function recurseCount(owed_$: number = precise(stillDue_$), index_$: number): void {
       if (owed_$ === 0 || index_$ < 0) {  // Stop recursion if no more money is owed,
@@ -123,10 +119,9 @@ export default function checkCashRegister(price: number, cash: number, cid: Arra
     recurseCount(stillDue_$, cid.length - 1);
     ///////////////////////////////////////
   }
-  if (precise(stillDue_$) > 0) { //old version: (stillDue_$ > 0.0001)
+  if (precise(stillDue_$) > 0) {
     // At this point, exact change cannot be given:
     // any bills or coins remaining in the till will be bigger than the amount due to the customer.
-    console.log('latter insufficient')
     return { status: 'INSUFFICIENT_FUNDS', change: [] };
   } else {
     // At this point, the exact amount of change is given to the customer and the till is ready for the next transaction
