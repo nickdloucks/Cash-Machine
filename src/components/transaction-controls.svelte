dc168c79c63666d79d3dcd48d079d6b4567a2aaf
Nick <nickdloucks@outlook.com>
2022-06-11 14:09:34 -0700

    transfer some styles from slotInterface

--- src/components/transaction-controls.svelte
+++ src/components/transaction-controls.svelte
@@ -9,7 +9,7 @@
 <section class="wrapper">
     <section class="grid-container main-sale">
         <span class="grid-item main-sale-in">
-            <label class="money-in-label" for="price">Price:</label>
+            <label class="slot-label" for="price">Price:</label>
             <svelte:component 
                 this={NumberBox} 
                 name={'price'} 
@@ -18,7 +18,7 @@
                 editable={true}></svelte:component>
         </span>
         <span class="grid-item main-sale-in">
-            <label class="money-in-label" for="paid">Paid:</label>
+            <label class="slot-label" for="paid">Paid:</label>
             <svelte:component 
                 this={NumberBox} 
                 name={'paid'} 
@@ -38,9 +38,6 @@
         background-image: linear-gradient(80deg, #818181, #313131, #111111);
         margin: 0;
     }
-    button{
-        width: max-content;
-    }
     .main-sale{
         margin: 0px;
         background-color: #000000;
@@ -48,14 +45,25 @@
         padding: 1.5em;
         height: max-content;
     }
-    .main-sale-in{
-        width: fit-content;
-    }
     .slot-value{
         font-size: 1em;
         font-family: 'Courier New', Courier, monospace;
         font-weight: bold;
         width: max-content;
         color: #00ff00;
+    }
+    .slot-label{
+        width: 4rem;
+        display: inline-block;
+        height: max-content;
+        background-color: #000;
+        border-radius: 3px;
+    }
+    .main-sale-in{
+        width: 5rem;
+        display: grid;
+        border-radius: 0.5em;
+        justify-content: center;
+        text-align: center;
     }
 </style>
